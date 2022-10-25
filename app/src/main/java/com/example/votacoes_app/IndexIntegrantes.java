92ff602fb73ea2ce08b22ce4728f29f0caf03caf
NicolasWeber0901 <nicolasweber0901@outlook.com>
2022-10-24 21:09:09 -0300

    Criada tela de cadastro de integrante

--- app/src/main/java/com/example/votacoes_app/IndexIntegrantes.java
+++ app/src/main/java/com/example/votacoes_app/IndexIntegrantes.java
@@ -2,7 +2,9 @@ package com.example.votacoes_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class IndexIntegrantes extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class IndexIntegrantes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index_integrantes);
+
+        Button btCadIntegrante = findViewById(R.id.btCadIntegrante);
+
+        btCadIntegrante.setOnClickListener(v ->{
+            Intent i = new Intent(IndexIntegrantes.this, CadastroIntegrante.class);
+            startActivity(i);
+        });
     }
 }
