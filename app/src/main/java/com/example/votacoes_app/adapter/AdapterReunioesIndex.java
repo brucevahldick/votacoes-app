c728a1ab678bc83e021d0a75bb9cfee84b651f69
Augusto Rustick <A.Rustick@outlook.com>
2022-12-04 22:15:09 -0300

    feat: exclusão de reuniões com modal

--- app/src/main/java/com/example/votacoes_app/adapter/AdapterReunioesIndex.java
+++ app/src/main/java/com/example/votacoes_app/adapter/AdapterReunioesIndex.java
@@ -1,12 +1,19 @@
 package com.example.votacoes_app.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import com.example.votacoes_app.CadastroReunioes;
+import com.example.votacoes_app.R;
+import com.example.votacoes_app.RedirectCards;
+import com.example.votacoes_app.TelaInicial;
 import com.example.votacoes_app.model.Reuniao;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -35,9 +42,37 @@ public class AdapterReunioesIndex extends BaseAdapterReunioes{
 
     @Override
     public void removeButtonEvent(Reuniao reuniao, int position){
+        final Dialog modal = new Dialog(context);
+
+        modal.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        modal.setCancelable(true);
+        modal.setContentView(R.layout.modal_remove_reuniao);
+
+        TextView data      = modal.findViewById(R.id.tvReuniaoDataModal);
+        TextView conselho  = modal.findViewById(R.id.tvReuniaoConselhoModal);
+
+        Button btCancelar  = modal.findViewById(R.id.btCancelarModalReuniao);
+        Button btConfirmar = modal.findViewById(R.id.btConfirmarModalReuniao);
+
+        data.setText(reuniao.getData());
+        conselho.setText(reuniao.getConselho());
+
+        btCancelar.setOnClickListener(v -> {
+            modal.hide();
+        });
+
+        btConfirmar.setOnClickListener(v -> {
+            confirmRemove(reuniao, position);
+            modal.hide();
+        });
+
+        modal.show();
+    }
+
+
+    public void confirmRemove(Reuniao reuniao, int position) {
         FirebaseFirestore.getInstance().collection("reuniao").document(reuniao.getId())
                 .delete();
-
 
         reunioes.remove(position);
         notifyItemRemoved(position);
